285237da4ee18501b1950d4a74858ec6a4993043
hp <hp@12983129837>
2017-11-24 14:35:12 +0630

    Update Street

--- GitHubExerscises/src/com/person/Person.java
+++ GitHubExerscises/src/com/person/Person.java
@@ -7,25 +7,36 @@ public class Person {
 
 	private String street;
 
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	
 
-	
 }
