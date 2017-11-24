3fac50911407bfa7de180d64bfa3f884206119b6
Administrator <Administrator@MR-SONT69GN1IE9>
2017-11-23 23:40:08 -0800

    add rollno

--- GitHubTesting/src/com/jdc/Exercises.java
+++ GitHubTesting/src/com/jdc/Exercises.java
@@ -4,6 +4,15 @@ public class Exercises {
 	private String test;
 	private String mark;
 	private String grade;
+	private int rollno;
+
+	public int getRollno() {
+		return rollno;
+	}
+
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
 
 	public String getTest() {
 		return test;
