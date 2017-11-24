63ef9c1ecb4b7542fb5db41416447f473ff7ff29
DELL <DELL@DESKTOP-G45M30J>
2017-11-24 11:53:18 -0800

    This is exercise!

---
+++ GitHubTesting/src/com/jdc/Exercises.java
@@ -0,0 +1,32 @@
+package com.jdc;
+
+public class Exercises {
+	private String test;
+	private String mark;
+	private String grade;
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+
+	public String getMark() {
+		return mark;
+	}
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+}
