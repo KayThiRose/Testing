3c58248b8e6584b09330351196e5d2ed63a5ae5b
hp <hp@12983129837>
2017-11-24 14:54:06 +0630

    Update count

--- Phone/src/com/kay/Phone.java
+++ Phone/src/com/kay/Phone.java
@@ -1,28 +1,42 @@
 package com.kay;
 
 public class Phone {
-	
+
 	private String name;
 	private String type;
 	private double price;
+	private int phonecount;
+
+	public int getPhonecount() {
+		return phonecount;
+	}
+
+	public void setPhonecount(int phonecount) {
+		this.phonecount = phonecount;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
 
 }
