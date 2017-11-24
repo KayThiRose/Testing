3dc4f53a2dd72a869a97ec7f13783d9a9c701814
DELL <DELL@DESKTOP-G45M30J>
2017-11-24 11:45:07 -0800

    add item

--- GitHubTesting/src/com/jdc/Testing.java
+++ GitHubTesting/src/com/jdc/Testing.java
@@ -6,6 +6,15 @@ public class Testing {
 	private int age;
 	private String address;
 	private String phone;
+	private String country;
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
 
 	public String getName() {
 		return name;
