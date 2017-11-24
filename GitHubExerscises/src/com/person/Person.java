07369b68614236b177824a7755ae8c891eb8f45d
Administrator <Administrator@MR-SONT69GN1IE9>
2017-11-23 23:18:55 -0800

    Hey

--- GitHubExerscises/src/com/person/Person.java
+++ GitHubExerscises/src/com/person/Person.java
@@ -3,30 +3,26 @@ package com.person;
 public class Person {
 	private String name;
 	private int age;
-	private int address;
-
-	public int getAddress() {
-		return address;
-	}
-
-	public void setAddress(int address) {
-		this.address = address;
-	}
-
+	private String address;
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public int getAge() {
 		return age;
 	}
-
 	public void setAge(int age) {
 		this.age = age;
 	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
 
+	
 }
