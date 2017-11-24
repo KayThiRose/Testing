662d1230a0c233a24063ce0d4611ce4eb6d3d3c4
DELL <DELL@DESKTOP-G45M30J>
2017-11-24 11:40:04 -0800

    Hey, I posted testing!

---
+++ GitHubTesting/src/com/jdc/Testing.java
@@ -0,0 +1,42 @@
+package com.jdc;
+
+public class Testing {
+
+	private String name;
+	private int age;
+	private String address;
+	private String phone;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
